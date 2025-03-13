6c290e2cc8f3d83d6ad692d94e8a13891ed3e0e6
neubeheer <neubeheer@gmail.com>
2025-03-13 20:56:56 -0300

    Create feedback.php

---
+++ feedback.php
@@ -0,0 +1,41 @@
+<?php
+// Cabeçalho para definir o tipo de conteúdo como HTML
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <style type="text/css">
+        /* Estilos CSS mantidos do original */
+        ol{margin:0;padding:0}table td,table th{padding:0}.c3{border-right-style:solid;padding:5pt 5pt 5pt 5pt;border-bottom-color:#000000;border-top-width:1pt;border-right-width:1pt;border-left-color:#000000;vertical-align:top;border-right-color:#000000;border-left-width:1pt;border-top-style:solid;border-left-style:solid;border-bottom-width:1pt;width:56.4pt;border-top-color:#000000;border-bottom-style:solid}.c5{border-right-style:solid;padding:5pt 5pt 5pt 5pt;border-bottom-color:#000000;border-top-width:1pt;border-right-width:1pt;border-left-color:#000000;vertical-align:top;border-right-color:#000000;border-left-width:1pt;border-top-style:solid;border-left-style:solid;border-bottom-width:1pt;width:64.5pt;border-top-color:#000000;border-bottom-style:solid}.c14{color:#980000;font-weight:700;text-decoration:none;vertical-align:baseline;font-size:7pt;font-family:"Arial";font-style:normal}.c6{color:#980000;font-weight:700;text-decoration:none;vertical-align:baseline;font-size:6pt;font-family:"Arial";font-style:normal}.c0{color:#000000;font-weight:700;text-decoration:none;vertical-align:baseline;font-size:10pt;font-family:"Arial";font-style:normal}.c12{color:#000000;font-weight:400;text-decoration:none;vertical-align:baseline;font-size:7pt;font-family:"Arial";font-style:normal}.c16{color:#000000;font-weight:700;text-decoration:none;vertical-align:baseline;font-size:18pt;font-family:"Arial";font-style:normal}.c10{padding-top:0pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:left}.c1{padding-top:0pt;padding-bottom:0pt;line-height:1.15;orphans:2;widows:2;text-align:center}.c8{text-decoration-skip-ink:none;font-size:10pt;-webkit-text-decoration-skip:none;font-weight:700;text-decoration:underline}.c13{padding-top:0pt;padding-bottom:0pt;line-height:1.0;text-align:left}.c11{border-spacing:0;border-collapse:collapse;margin-right:auto}.c4{padding-top:0pt;padding-bottom:0pt;line-height:1.0;text-align:center}.c19{text-decoration:none;vertical-align:baseline;font-family:"Arial";font-style:normal}.c17{background-color:#ffffff;max-width:451.4pt;padding:72pt 72pt 72pt 72pt}.c18{color:#274e13;font-size:8pt;font-weight:700}.c9{color:#980000;font-size:6pt;font-weight:700}.c15{font-size:10pt;font-weight:700}.c2{height:11pt}.c7{height:0pt}.title{padding-top:0pt;color:#000000;font-size:26pt;padding-bottom:3pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}.subtitle{padding-top:0pt;color:#666666;font-size:15pt;padding-bottom:16pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}li{color:#000000;font-size:11pt;font-family:"Arial"}p{margin:0;color:#000000;font-size:11pt;font-family:"Arial"}h1{padding-top:20pt;color:#000000;font-size:20pt;padding-bottom:6pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h2{padding-top:18pt;color:#000000;font-size:16pt;padding-bottom:6pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h3{padding-top:16pt;color:#434343;font-size:14pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h4{padding-top:14pt;color:#666666;font-size:12pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h5{padding-top:12pt;color:#666666;font-size:11pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;orphans:2;widows:2;text-align:left}h6{padding-top:12pt;color:#666666;font-size:11pt;padding-bottom:4pt;font-family:"Arial";line-height:1.15;page-break-after:avoid;font-style:italic;orphans:2;widows:2;text-align:left}
+    </style>
+</head>
+<body class="c17 doc-content">
+    <div>
+        <p class="c1"><span class="c16">FEEDBACK - OMINI/TALK</span></p>
+        <table class="c11">
+            <tr class="c7">
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">TME</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">TMA</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">TMIA</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">AB/PROD</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">PENDÊNCIAS</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">O.S/LOG</span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c4"><span class="c14">PERÍODO</span></p></td>
+            </tr>
+            <tr class="c7">
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c13 c2"><span class="c12"></span></p></td>
+                <td class="c5" colspan="1" rowspan="1"><p class="c2 c13"><span class="c12"></span></p></td>
+            </tr>
+        </table>
+        <p class="c2 c10"><span class="c12"></span></p>
+    </div>
+    <!-- Restante do conteúdo HTML -->
+</body>
+</html>
